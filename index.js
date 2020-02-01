24f20de0acc29721d824f6c6238feebf37758731
Pranavi Choudhary <ppch09@gmail.com>
2020-02-02 01:42:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,13 @@ mqttServ.attachHttpServer(httpServ);
 
 httpServ.listen(process.env.PORT || 8080);
 
-const Pool = require('pg'); 
-const secrets = require('../middleware/ENV').default;
-const env = process.env.NODE_ENV || 'development';
-let connectionString = {
-    user: secrets.user,
-    database: secrets.testDb,
-    host: secrets.host
-};
-// checking to know the environment and suitable connection string to use
-if (env === 'development') {
-    connectionString.database = secrets.database;
-} else {
-    connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-    };
-};
-const pool = new Pool(connectionString);
-pool.on('connect', () => console.log('connected to db'));
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+//client.connect();
+client.on('connect', () => console.log('connected to db'));
