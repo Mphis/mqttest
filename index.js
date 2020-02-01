b5ba38dcef86bcc34d905ec86258e086bb6bafc5
Pranavi Choudhary <ppch09@gmail.com>
2020-02-02 02:35:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http'),
     httpServ = http.createServer(),
     mosca = require('mosca'),
+    mqtt    = require('mqtt'),
     mqttServ = new mosca.Server({});
 
 mqttServ.attachHttpServer(httpServ);
@@ -15,7 +16,6 @@ const client = new Client({
   ssl: true,
 });
 console.log('connecting');
-
 client.connect();
 console.log('connected');
 
@@ -26,3 +26,17 @@ client.query('SELECT data_a FROM topic1;', (err, res) => {
   }
   client.end();
 });
+
+var client_mqtt  = mqtt.connect('ws://mqtt-broker-hackit.herokuapp.com');
+
+client_mqtt.on('connect', function () {
+    client_mqtt.subscribe('presence')
+})
+client_mqtt.on('message', function (topic, message) {
+    context = message.toString();
+    client.query('INSERT INTO topic1 (data_a) VALUES ('+context+');', (err, res) => {
+      if (err) throw err;
+      client.end();
+    });
+console.log(context)
+})
